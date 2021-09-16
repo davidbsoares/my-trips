6aacc87cc9adce428fa7afe98af5ac5ecbbd72c1
David Brum Soares <david.bsoares@hotmail.com>
2021-09-16 00:25:40 -0300

    Finished

---
+++ next-seo.config.ts
@@ -0,0 +1,8 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_us',
+    url: 'https://my-trips.davidbsoares.com.br',
+    site_name: 'SiteName',
+  }
+};
---
+++ src/components/Map/test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react'
+
+import Map from '.'
+
+describe('<Map />', () => {
+  it('should render without any marker', () => {
+    render(<Map />)
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument
+  })
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Brasília',
+      slug: 'brasilia',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+    const placeTwo = {
+      id: '2',
+      name: 'Rio de Janeiro',
+      slug: 'rio_de_janeiro',
+      location: {
+        latitude: 100,
+        longitude: 100
+      }
+    }
+    render(<Map places={[place, placeTwo]} />)
+    expect(screen.getByTitle(/brasília/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/rio de janeiro/i)).toBeInTheDocument()
+  })
+})
